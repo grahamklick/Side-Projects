790e71465da710903068ad15625d4800aadc2549
grahamklick <grahamklick@gmail.com>
2026-04-11 12:46:42 -0500

    Add CSV cleaner CLI project

---
+++ csv-cleaner-cli/README.md
@@ -0,0 +1,40 @@
+\# CSV Cleaner CLI
+
+
+
+A command-line Python tool that reads a CSV file, removes duplicate rows and missing values, and writes a cleaned output file.
+
+
+
+\## Tools Used
+
+\- Python
+
+\- pandas
+
+
+
+\## How to Run
+
+
+
+Install pandas:
+
+
+
+```bash
+
+pip install pandas
+
+
+
+Run the Script:
+
+python app.py data.csv
+
+
+
+Or, Point to another file:
+
+python app.py ../csv-cleaner/data.csv
+
---
+++ csv-cleaner-cli/app.py
@@ -0,0 +1,31 @@
+import pandas as pd #imports pandas, alias of pd for function calls
+import sys #gives python system access, extra variables
+
+#Determine total number of command-line arguments, including the script name. 
+#Exit if the script is the only argument passed.
+if len(sys.argv) < 2:
+    print("Usage: python app.py <input_file>")
+    sys.exit(1)
+
+#Capture filename or path being passed
+input_file = sys.argv[1]
+
+# Attempt to read the file, if none present error & exit application
+try:
+     df = pd.read_csv(input_file)
+except Exception as e:
+    print(f"Error reading file: {e}")
+    sys.exit(1)
+
+#Print first N rows of original data
+print("Original data:")
+print(df.head())
+
+#Drop duplicate rows, rows with missing column data
+df = df.drop_duplicates()
+df = df.dropna()
+
+output_file = "cleaned_" + input_file
+df.to_csv(output_file, index=False)
+
+print(f"\nCleaned file saved as {output_file}")
