f1216f0d36b06db2de86eca210939b7775ada10c
grahamklick <grahamklick@gmail.com>
2026-04-15 19:46:38 -0500

    Added CLI argument to data-pipeline-mini extract/transform file

--- data-pipeline-mini/extract_transform.py
+++ data-pipeline-mini/extract_transform.py
@@ -1,9 +1,23 @@
 import pandas as pd
+import sys
+import os
 
-#replaced prior sample data with data from external csv
-df = pd.read_csv("raw_sales.csv")
+#validate >= 2 arguments or prompt reminder and exit
+#run in terminal like: python extract_transform.py raw_sales.cv --- runs the .py file, using the raw_sales.csv file = 2 arguments
+if len(sys.argv) < 2:
+    print("Usage: python extract_transform.py <input_file> [output_file]")
+    sys.exit(1)
+    
+input_file = sys.argv[1]
 
-#display initial data
+if len(sys.argv) >= 3:
+    output_file = sys.argv[2]
+else:
+    output_file = "cleaned_" + os.path.basename(input_file) # using os.path to overcome file folder issues experienced in earlier projects
+
+#Load Data
+df = pd.read_csv(input_file) #replaced hard coded file name, now can find based on input file provided, test using the raw_sales csv
+
 print("Raw data:")
 print(df)
 
